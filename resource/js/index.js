85cf66e526a6c0cf3c036e5eb664ed07e93f4636
pdw <pdw@pdwui-mbp.korea.ncsoft.corp>
2015-08-13 11:16:28 +0900

    category

--- resource/js/index.js
+++ resource/js/index.js
@@ -7,7 +7,7 @@ module.exports = {
   init: function(){
     var _tmp = [], selectCategory = common.selectCategory();
     selectCategory.then( function( data ){
-      var template = '<a href="/list.html?product={{name}}" class="list-group-item">{{name}}</a>';
+      var template = '<a href="list.html?product={{name}}" class="list-group-item">{{name}}</a>';
       if( data.result === 'success'){
         var items = data.data;
 
---
+++ tests/55c4556181e7975c10d88e27.js
@@ -0,0 +1,28 @@
+var waitTime = 5000;
+
+module.exports = {
+
+
+  'bns-main-login' : function (browser) {
+
+    browser
+      .url('http://rc.bns.plaync.com')
+
+      .waitForElementVisible('body', waitTime)
+
+      .assert.title('plaync 블레이드 & 소울')
+
+      .setValue('#id', 'bnswmc001')
+
+      .setValue('#pwd', 'aaaa1111')
+
+      .click('#login')
+
+      .waitForElementVisible('body', waitTime)
+
+      .assert.containsText( 'span.gnbMyname', '알씨일이삼사오육칠팔구십일이삼사' )
+      
+      .end();
+
+  }
+};
